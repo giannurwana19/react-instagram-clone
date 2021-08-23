050e3d30578c9eab43ea0ff709d446676bcffd0f
giannurwana19 <giannurwana19@gmail.com>
2021-08-23 11:03:00 +0700

    sign in & sign up firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import { useEffect, useState } from 'react';
+import { Button, Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
 import './App.css';
 import { logo } from './assets';
-import { db } from './firebase';
+import { auth, db } from './firebase';
 import Post from './Post';
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [modalStyle] = useState(getModalStyle);
+  const classes = useStyles();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        // user has login
+        setUser(authUser);
+        console.log(authUser);
+      } else {
+        // user has logout
+        setUser(null);
+        console.log(authUser);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [username]);
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
@@ -20,12 +49,93 @@ function App() {
     });
   }, []);
 
+  const signUp = e => {
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(authUser => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch(err => alert(err.message));
+  };
+
+  const signIn = e => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch(err => alert(err.message));
+
+    setOpenSignIn(false);
+  };
+
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form onSubmit={signUp} className="app-signup">
+            <center>
+              <img src={logo} className="app-header-image" alt="" />
+            </center>
+            <Input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+            />
+            <Input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <Button type="submit">Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form onSubmit={signIn} className="app-signup">
+            <center>
+              <img src={logo} className="app-header-image" alt="" />
+            </center>
+            <Input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+            <Button type="submit">Sign In</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app-header">
         <img src={logo} alt="logo" />
       </div>
-      <h1>Hello</h1>
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+      ) : (
+        <div className="app-login-container">
+          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+        </div>
+      )}
+
       {posts.map(({ post, id }) => (
         <Post
           key={id}
@@ -37,6 +147,28 @@ function App() {
     </div>
   );
 }
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 export default App;
 
